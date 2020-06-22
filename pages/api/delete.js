0f6dea273b60e6d49eb5cf54a6b57444e9d84089
Guram <gurikakhasia@gmail.com>
2020-06-22 14:05:01 +0400

    Improved API responses

--- pages/api/delete.js
+++ pages/api/delete.js
@@ -1,16 +1,25 @@
-import { deletePost } from "../../lib/db";
+import { deletePost, hasId } from "../../lib/db";
 
-export default (req, res) => {
+export default async (req, res) => {
   let id = parseInt(req.body.id);
-  if (!id) {
+
+  if (!id)
     res.status(405).json({
       msg: "id is not specified",
     });
-  } else {
+
+  if (await hasId(id)) {
     deletePost(id);
+
     res.status(200).json({
       id: id,
       msg: `post with id:${id} is DELETED`,
+    });
+
+  } else {
+    res.status(405).json({
+      id: id,
+      msg: `ERROR post with id:${id} does not exists`,
     });
   }
 };
--- pages/api/put.js
+++ pages/api/put.js
@@ -1,21 +1,25 @@
-import { updatePost } from "../../lib/db";
-export default  async (req, res) => {
-  let { id,heading,content } = req.body;
-  console.log(req.body)
+import { updatePost, hasId } from "../../lib/db";
+export default async (req, res) => {
+  let { id, heading, content } = req.body;
+  console.log(req.body);
 
-  parseInt(id)
+  parseInt(id);
 
   if (!id || !heading || !content) {
     res.status(405).json({
       msg: "all fields must be filled",
     });
   }
-  else {
-    console.log(id)
-    await updatePost(id,heading,content)
+  if (await hasId(id)) {
+    await updatePost(id, heading, content);
     res.status(200).json({
-      msg:`post with id:${id} updated`
-    })
+      msg: `post with id:${id} updated`,
+    });
   }
-
+  else{
+    res.status(405).json({
+      id:id,
+      msg: `ERROR ! post with id:${id} does not exists`,
+    });
+  }
 };
