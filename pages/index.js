bc15c1fc1f99aed0590cfe6faacf107d9088c8f8
Guram <gurikakhasia@gmail.com>
2020-06-28 22:31:30 +0400

    Fixed parsed content issue

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,10 @@ export default function Home({ postsArray }) {
             <article className="single-blog">
               <div>
                 <a className="post-heading">{post.heading}</a>
-                {parse(post.content) || parse(post.content).slice(0, 3)}
+                {console.log(parse(post.content))}
+                {parse(post.content).length >= 1
+                  ? parse(post.content).slice(0, 3)
+                  : parse(post.content)}
                 <div className="post-id">პოსტი: {post.id}</div>
               </div>
             </article>
